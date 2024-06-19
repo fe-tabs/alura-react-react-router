d084f0046dad1e3dfefc3c174c4d27e875b2dc4d
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-19 19:52:20 -0300

    :sparkles: feat: Footer component and Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Menu from "./components/Menu";
-import Home from "./pages/Home";
-import AboutMe from "./pages/AboutMe";
+import Menu from "components/Menu";
+import Home from "pages/Home";
+import AboutMe from "pages/AboutMe";
+import Footer from "components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="/about-me" element={<AboutMe/>} />
         <Route path="*" element={<div>Página não encontrada</div>}/>
       </Routes>
+
+      <Footer/>
     </BrowserRouter>
   );
 }
