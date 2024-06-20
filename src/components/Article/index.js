00c56343351b879fd7371b000e51d9a3a6f36e0e
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-19 23:38:13 -0300

    :bug: fix: adjust overflowing image

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -8,9 +8,11 @@ export default function Article({ cover, title, children }) {
         <img className={styles['article__cover__image']} src={cover}/>
       </div>
 
-      <h2 className={styles['article__title']}>{title}</h2>
-    
-      <div className={styles['article__content']}>{children}</div>
+      <div className={styles['article__container']}>
+        <h2 className={styles['article__title']}>{title}</h2>
+      
+        <div className={styles['article__content']}>{children}</div>
+      </div>
     </article>
   );
 }
